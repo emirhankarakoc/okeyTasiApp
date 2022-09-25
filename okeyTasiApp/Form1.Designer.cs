0e30b63570729a360b04ed567b7856c000208791
emirhankarakoc <emirhankarakoc@yahoo.com>
2022-09-25 14:36:07 +0300

    Proje dosyası ekle.

---
+++ okeyTasiApp/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace okeyTasiApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 389);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(211, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 389);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox3.Location = new System.Drawing.Point(366, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 389);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox4.Location = new System.Drawing.Point(514, 22);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 389);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(79, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "13";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(79, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "12";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(79, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "11";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(79, 223);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "10";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(79, 283);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "9";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(79, 340);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "8";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 450);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "101 tas sayma programi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(79, 379);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 23);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox2.Location = new System.Drawing.Point(119, 379);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 23);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox3.Location = new System.Drawing.Point(265, 379);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 23);
+            this.textBox3.TabIndex = 20;
+            this.textBox3.Text = "2";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(225, 379);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(35, 23);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "1";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(225, 340);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "8";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(225, 283);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "9";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(225, 223);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "10";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button10.Location = new System.Drawing.Point(225, 163);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "11";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button11.Location = new System.Drawing.Point(225, 104);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "12";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button12.Location = new System.Drawing.Point(225, 49);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "13";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(694, 474);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+    }
+}
---
+++ okeyTasiApp/Form1.cs
@@ -0,0 +1,284 @@
+namespace okeyTasiApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int buton1Sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buton1Sayac++;
+            if (buton1Sayac == 1)
+            {
+                button1.BackColor = Color.LightGreen;
+                button1.ForeColor = Color.Black;
+            }
+            else if (buton1Sayac == 2)
+            {
+                button1.BackColor = Color.DarkGreen;
+                button1.ForeColor = Color.Yellow;
+
+            }
+            else if (buton1Sayac == 3) {
+                buton1Sayac = 0;
+                button1.BackColor = DefaultBackColor;
+                button1.ForeColor = DefaultForeColor;
+            }
+        }
+
+
+        int buton2Sayac = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buton2Sayac++;
+
+            if (buton2Sayac == 1) {
+                button2.BackColor = Color.LightGreen;
+                button2.ForeColor = Color.Black;
+            }
+            else if (buton2Sayac == 2)
+            {
+                button2.BackColor = Color.DarkGreen;
+                button2.ForeColor = Color.Yellow;
+
+            }
+            else if (buton2Sayac == 3)
+            {
+                buton2Sayac = 0;
+                button2.BackColor = DefaultBackColor;
+                button2.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton3Sayac = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buton3Sayac++;
+            if (buton3Sayac == 1) {
+                button3.BackColor = Color.LightGreen;
+                button3.ForeColor = Color.Black;
+            }
+            else if (buton3Sayac == 2)
+            {
+                button3.BackColor = Color.DarkGreen;
+                button3.ForeColor = Color.Yellow;
+
+            }
+            else if (buton3Sayac == 3)
+            {
+                buton3Sayac = 0;
+                button3.BackColor = DefaultBackColor;
+                button3.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton4Sayac = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            buton4Sayac++;
+            if (buton4Sayac == 1)
+            {
+                button4.BackColor = Color.LightGreen;
+                button4.ForeColor = Color.Black;
+            }
+            else if (buton4Sayac == 2)
+            {
+                button4.BackColor = Color.DarkGreen;
+                button4.ForeColor = Color.Yellow;
+
+            }
+            else if (buton4Sayac == 3)
+            {
+                buton4Sayac = 0;
+                button4.BackColor = DefaultBackColor;
+                button4.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton5Sayac=0;
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            buton5Sayac++;
+            if (buton5Sayac == 1)
+            {
+                button5.BackColor = Color.LightGreen;
+                button5.ForeColor = Color.Black;
+            }
+            else if (buton5Sayac == 2)
+            {
+                button5.BackColor = Color.DarkGreen;
+                button5.ForeColor = Color.Yellow;
+
+            }
+            else if (buton5Sayac == 3)
+            {
+                buton5Sayac = 0;
+                button5.BackColor = DefaultBackColor;
+                button5.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton6Sayac = 0;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            buton6Sayac++;
+            if (buton6Sayac == 1)
+            {
+                button6.BackColor = Color.LightGreen;
+                button6.ForeColor = Color.Black;
+            }
+            else if (buton6Sayac == 2)
+            {
+                button6.BackColor = Color.DarkGreen;
+                button6.ForeColor = Color.Yellow;
+
+            }
+            else if (buton6Sayac == 3)
+            {
+                buton5Sayac = 0;
+                button6.BackColor = DefaultBackColor;
+                button6.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton12Sayac = 0;
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+            buton12Sayac++;
+            if (buton12Sayac == 1)
+            {
+                button12.BackColor = Color.LightGreen;
+                button12.ForeColor = Color.Black;
+            }
+            else if (buton12Sayac == 2)
+            {
+                button12.BackColor = Color.DarkGreen;
+                button12.ForeColor = Color.Yellow;
+
+            }
+            else if (buton12Sayac == 3)
+            {
+                buton12Sayac = 0;
+                button12.BackColor = DefaultBackColor;
+                button12.ForeColor = DefaultForeColor;
+            }
+        }
+            int buton11Sayac = 0;
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            buton11Sayac++;
+            if (buton11Sayac == 1)
+            {
+                button11.BackColor = Color.LightGreen;
+                button11.ForeColor = Color.Black;
+            }
+            else if (buton11Sayac == 2)
+            {
+                button11.BackColor = Color.DarkGreen;
+                button11.ForeColor = Color.Yellow;
+
+            }
+            else if (buton11Sayac == 3)
+            {
+                buton11Sayac = 0;
+                button11.BackColor = DefaultBackColor;
+                button11.ForeColor = DefaultForeColor;
+            }
+        }
+
+        int buton10Sayac = 0;
+        private void button10_Click(object sender, EventArgs e)
+        {
+            buton10Sayac++;
+            if (buton10Sayac == 1)
+            {
+                button10.BackColor = Color.LightGreen;
+                button10.ForeColor = Color.Black;
+            }
+            else if (buton10Sayac == 2)
+            {
+                button10.BackColor = Color.DarkGreen;
+                button10.ForeColor = Color.Yellow;
+
+            }
+            else if (buton10Sayac == 3)
+            {
+                buton10Sayac = 0;
+                button10.BackColor = DefaultBackColor;
+                button10.ForeColor = DefaultForeColor;
+            }
+
+        }
+        int buton9Sayac = 0;
+        private void button9_Click(object sender, EventArgs e)
+        {
+            buton9Sayac++;
+            if (buton9Sayac == 1)
+            {
+                button9.BackColor = Color.LightGreen;
+                button9.ForeColor = Color.Black;
+            }
+            else if (buton9Sayac == 2)
+            {
+                button9.BackColor = Color.DarkGreen;
+                button9.ForeColor = Color.Yellow;
+
+            }
+            else if (buton9Sayac == 3)
+            {
+                buton9Sayac = 0;
+                button9.BackColor = DefaultBackColor;
+                button9.ForeColor = DefaultForeColor;
+            }
+
+        }
+        int buton8Sayac = 0;
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            buton8Sayac++;
+            if (buton8Sayac == 1)
+            {
+                button8.BackColor = Color.LightGreen;
+                button8.ForeColor = Color.Black;
+            }
+            else if (buton8Sayac == 2)
+            {
+                button8.BackColor = Color.DarkGreen;
+                button8.ForeColor = Color.Yellow;
+
+            }
+            else if (buton8Sayac == 3)
+            {
+                buton8Sayac = 0;
+                button8.BackColor = DefaultBackColor;
+                button8.ForeColor = DefaultForeColor;
+            }
+        }
+        int buton7Sayac = 0;
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buton7Sayac++;
+            if (buton7Sayac == 1)
+            {
+                button7.BackColor = Color.LightGreen;
+                button7.ForeColor = Color.Black;
+            }
+            else if (buton7Sayac == 2)
+            {
+                button7.BackColor = Color.DarkGreen;
+                button7.ForeColor = Color.Yellow;
+
+            }
+            else if (buton7Sayac == 3)
+            {
+                buton7Sayac = 0;
+                button7.BackColor = DefaultBackColor;
+                button7.ForeColor = DefaultForeColor;
+            }
+
+        }
+    }
+}
